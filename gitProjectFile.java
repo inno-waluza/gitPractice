580ef87ca6872629593f4c6148813956c0d35d50
innowaluza <innowaluza@gmail.com>
2023-02-10 02:11:48 -0800

    added one sys.out in child branch

--- gitProjectFile.java
+++ gitProjectFile.java
@@ -3,5 +3,6 @@ class gitProjectFile{
     public static void main (String[] args){
         System.out.println("hello git");
         System.out.print("lets commit the changes");
+        System.out.println("we're now in child branch");
     }
 }
