1c679c5638122f66025d4aee5675b9ecdaa9af73
Innocent Waluza <86979309+inno-waluza@users.noreply.github.com>
2023-02-11 05:30:20 -0800

    Update gitProjectFile.java
    just making some changes

--- gitProjectFile.java
+++ gitProjectFile.java
@@ -5,5 +5,6 @@ class gitProjectFile{
         System.out.println("lets commit the changes");
         System.out.println("we're now in child branch");
         System.out.println("now we are on the cloud");
+        System.out.println("Lets use the pull command boys");
     }
 }
