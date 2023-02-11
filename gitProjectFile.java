52ffc8873e12554b612d33ca0c0b30c8c3299759
Innocent Waluza <86979309+inno-waluza@users.noreply.github.com>
2023-02-11 05:23:08 -0800

    Update gitProjectFile.java
    we are now on the cloud guys

--- gitProjectFile.java
+++ gitProjectFile.java
@@ -2,7 +2,8 @@ class gitProjectFile{
 
     public static void main (String[] args){
         System.out.println("hello git");
-        System.out.print("lets commit the changes");
+        System.out.println("lets commit the changes");
         System.out.println("we're now in child branch");
+        System.out.println("now we are on the cloud");
     }
 }
