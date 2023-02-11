d51f6e03bc49f471097a98585156c76e013a8efb
innowaluza <innowaluza@gmail.com>
2023-02-11 05:43:50 -0800

    added one method

--- gitProjectFile.java
+++ gitProjectFile.java
@@ -1,10 +1,19 @@
 class gitProjectFile{
 
+    public void acuteMethod(){
+        System.out.println("what is an acute angle");
+    }
+
     public static void main (String[] args){
         System.out.println("hello git");
         System.out.println("lets commit the changes");
         System.out.println("we're now in child branch");
         System.out.println("now we are on the cloud");
         System.out.println("Lets use the pull command boys");
+
+        /*lets call the method */
+        gitProjectFile git = new gitProjectFile();
+
+        git.acuteMethod();
     }
 }
